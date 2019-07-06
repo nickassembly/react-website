c3ec4d36405174d63ca76937bf782f2c1de04203
Nick Guerra <nickg@maliciousgames.com>
2019-07-06 08:44:03 -0500

    page w styled components

---
+++ src/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Contact = () => (
+  <div>
+    <h2>Contact Us</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut iusto
+      quibusdam accusantium commodi totam officiis dolor reprehenderit
+      necessitatibus veniam sint.
+    </p>
+  </div>
+);
---
+++ src/home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Home = () => (
+  <div>
+    <h2>Hello World</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut iusto
+      quibusdam accusantium commodi totam officiis dolor reprehenderit
+      necessitatibus veniam sint.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ut facere e
+      impedit neque aut hic atque cumque vel veritatis voluptas? Nihil quasi
+      earu
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
+      necessitatibus illo cum repellendus beatae optio consectetur error
+      praesentium molestias facere.
+    </p>
+  </div>
+);
